30562e9cebc436b48fc4a69447b38721189d95b3
Brian Manley <brian.manley@wework.com>
2022-10-10 08:21:31 -0400

    Better (and working) solution

--- src/word-truncate/truncate.test.ts
+++ src/word-truncate/truncate.test.ts
@@ -2,14 +2,14 @@ import { truncate } from './truncate';
 
 
 describe ('Word Truncate', () => {
-    // it ('should remove letters after the nth letter in each word', () => {
-    //     const input = 'never gonna give you up';
-    //     const expected = 'nev gon giv you up';
+    it ('should remove letters after the nth letter in each word', () => {
+        const input = 'never gonna give you up';
+        const expected = 'nev gon giv you up';
 
-    //     const actual = truncate(input, 3);
+        const actual = truncate(input, 3);
 
-    //     expect(actual).toEqual(expected);
-    // });
+        expect(actual).toEqual(expected);
+    });
 
     it ('should ignore non-alpabetical characters', () => {
         const input = '*hello* darkness, my ~old_friend';
--- src/word-truncate/truncate.ts
+++ src/word-truncate/truncate.ts
@@ -1,28 +1,29 @@
 export const truncate = (inputString: string, n: number): string => {
-    // Easy but slower approach
-    // Split input into words
-    const words = inputString.split(' ');
-    // Shorten each word
-    const shortenedWords = words.map(word => shortenWord(word, n));
-    // Join words back into return string
-    return shortenedWords.join(' ');
-};
-
-const shortenWord = (input: string, length: number): string => {
-    let charCounter = 0;
+    // Walk through input string
+    // Keep track of how many letters we've seen since the last delimiter
+    // If we're on a letter and the char counter is under the target number, append the char to the output
+    // If we hit a delimiter, add the current char to the output and reset the char counter
+    // Otherwise, just add the current char to the output
     let output = '';
-    for (let i = 0; i < input.length; i++) {
-        const currentChar = input.charAt(i);
+    let charCounter = 0;
+    for (let i = 0; i < inputString.length; i++) {
+        const currentChar = inputString.charAt(i);
         if (isLetter(currentChar)) {
-            if (charCounter < length) {
+            if (charCounter < n) {
                 output += currentChar;
                 charCounter++;
             }
+        } else if (DELIMITERS.has(currentChar)) {
+            output += currentChar;
+            charCounter = 0;
         } else {
             output += currentChar;
         }
     }
+    
     return output;
 };
 
 const isLetter = (input: string): boolean => input.length === 1 && input.toLocaleLowerCase() != input.toLocaleUpperCase();
+
+const DELIMITERS = new Set([' ', '_']);
