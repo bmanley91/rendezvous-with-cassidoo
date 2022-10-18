ca8340962bf963ac3878f074443d4fc885e49df5
Brian Manley <brian.manley@wework.com>
2022-10-18 18:31:54 -0400

    O(Doors * Passes) approach

--- src/pass-doors/pass-doors.ts
+++ src/pass-doors/pass-doors.ts
@@ -1,3 +1,18 @@
 export const passDoors = (doors: number, passes: number) => {
-    return doors * passes;
+    // For each door, increment from 1 to number of passes, 
+    // see if the index is divisible by the pass number
+    // If it is, its state flips.
+    // Doors start with index 1
+    let openCount = 0;
+    for (let doorNumber = 1; doorNumber <= doors; doorNumber++) {
+        let doorOpen = false;
+        for (let passNumber = 1; passNumber <= passes; passNumber++) {
+            if (doorNumber % passNumber === 0) {
+                doorOpen = !doorOpen;
+            }
+        }
+        if (doorOpen) openCount++;
+    }
+
+    return openCount;
 };
