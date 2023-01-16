08802ddbaed062d69a7d0c6ec4c67cabf6e9e75d
Brian Manley <brian.manley91@gmail.com>
2023-01-16 18:05:25 -0500

    Finished spint the wheel

--- src/spin-the-wheel/spin-the-wheel.test.ts
+++ src/spin-the-wheel/spin-the-wheel.test.ts
@@ -1,39 +1,45 @@
-import { getScoreReport, initializePlayerList, Player } from './spin-the-wheel';
+import { getScoreReport, initializePlayerList, main, Player } from './spin-the-wheel';
 
-describe ('Initialize Players', () => {
-    it ('returns a list of players of the given size', () => {
-        const expected = [
-            new Player('Player0', 10),
-            new Player('Player1', 10),
-            new Player('Player2', 10),
-        ];
-
-        const actual = initializePlayerList(3);
-
-        actual.forEach((actualPlayer, index) => {
-            expect(actualPlayer.name).toBe(expected[index].name);
-            expect(actualPlayer.score).toBe(expected[index].score);
-        });
-    });
+describe('Play the game', () => {
+    it ('Runs the game', () =>
+        main()
+    );
 });
 
-describe ('Get Score Report', () => {
-    it ('returns a report of players and their scores', () => {
-        const expected = 'Round 5 Scores:\n' +
-            'name: Player1, score: 10\n' +
-            'name: Player2, score: 8\n' +
-            'name: Player3, score: 0\n' +
-            'name: Player4, score: 999';
-        
-        const playerList = [
-            new Player('Player1', 10),
-            new Player('Player2', 8),
-            new Player('Player3', 0),
-            new Player('Player4', 999),
-        ];
-        
-        const actual = getScoreReport(playerList, 5);
+// describe ('Initialize Players', () => {
+//     it ('returns a list of players of the given size', () => {
+//         const expected = [
+//             new Player('Player0', 10),
+//             new Player('Player1', 10),
+//             new Player('Player2', 10),
+//         ];
 
-        expect(actual).toEqual(expected);
-    });
-});
+//         const actual = initializePlayerList(3);
+
+//         actual.forEach((actualPlayer, index) => {
+//             expect(actualPlayer.name).toBe(expected[index].name);
+//             expect(actualPlayer.score).toBe(expected[index].score);
+//         });
+//     });
+// });
+
+// describe ('Get Score Report', () => {
+//     it ('returns a report of players and their scores', () => {
+//         const expected = 'Round 5 Scores:\n' +
+//             'name: Player1, score: 10\n' +
+//             'name: Player2, score: 8\n' +
+//             'name: Player3, score: 0\n' +
+//             'name: Player4, score: 999';
+        
+//         const playerList = [
+//             new Player('Player1', 10),
+//             new Player('Player2', 8),
+//             new Player('Player3', 0),
+//             new Player('Player4', 999),
+//         ];
+        
+//         const actual = getScoreReport(playerList, 5);
+
+//         expect(actual).toEqual(expected);
+//     });
+// });
--- src/spin-the-wheel/spin-the-wheel.ts
+++ src/spin-the-wheel/spin-the-wheel.ts
@@ -17,10 +17,16 @@ export class Player {
 }
 
 enum GameResult {
-    WIN,
-    LOSE,
-    KEEP
+    WIN = 'WIN',
+    LOSE = 'LOSE',
+    KEEP = 'KEEP'
 }
+
+const wheel = [
+    GameResult.WIN,
+    GameResult.LOSE,
+    GameResult.KEEP
+];
 
 export const initializePlayerList = (playerCount: number): Player[] => {
     const playerList = [];
@@ -30,12 +36,47 @@ export const initializePlayerList = (playerCount: number): Player[] => {
     return playerList;
 };
 
-const playGame = (playerList: Player[]): Player[] => {
+const playGame = (playerList: Player[], roundCount: number): Player[] => {
+    let runningPlayerStatus = playerList;
+    for (let roundNumber = 1; roundNumber <= roundCount; roundNumber++) {
+        console.log(`Playing round ${roundNumber}`);
+        runningPlayerStatus = playRound(runningPlayerStatus);
+        console.log(getScoreReport(runningPlayerStatus, roundNumber));
+    }
     return playerList;
 };
 
 const playRound = (playerList: Player[]): Player[] => {
-    return playerList;
+    let updatedPlayerStatus = playerList;
+
+    updatedPlayerStatus = playerList.map(player => {
+        if (player.score === 0) {
+            return player;
+        }
+
+        // Let's just use half of the player's score.
+        const wagerAmount = Math.ceil(player.score / 2);
+        console.log(`${player.name} wagers ${wagerAmount}`);
+        const scoreDiff = spinWheel(wagerAmount);
+
+        return new Player(player.name, player.score + scoreDiff);
+    });
+
+    return updatedPlayerStatus;
+};
+
+const spinWheel = (wager: number): number => {
+    const result = wheel[Math.floor(Math.random() * wheel.length)];
+    console.log(`Result is ${result}`);
+
+    switch (result) {
+    case GameResult.WIN:
+        return wager * 2;
+    case GameResult.LOSE:
+        return 0;
+    default:
+        return wager;
+    }
 };
 
 export const getScoreReport = (playerList: Player[], roundNumber: number): string => {
@@ -44,9 +85,19 @@ export const getScoreReport = (playerList: Player[], roundNumber: number): strin
     return stringBuilder.join('\n');
 };
 
-const main = (): void => {
+export const getFinalScore = (playerList: Player[]): string => {
+    const stringBuilder = ['Final Score:'];
+    playerList.forEach(player => stringBuilder.push(`${player}`));
+    return stringBuilder.join('\n');
+};
+
+export const main = (): void => {
     // Set up players
     const playerList = initializePlayerList(gameSetup.playerCount);
+
+    const finalResults = playGame(playerList, gameSetup.roundCount);
+
+    console.log(getFinalScore(finalResults));
 };
 
 main();
