94ec3fb1a30c914f94bef06fcd33a57371f222d8
Brian Manley <brian.manley@wework.com>
2023-01-23 08:03:32 -0500

    Finished missing bits

--- src/missing-bits/missing-bits.test.ts
+++ src/missing-bits/missing-bits.test.ts
@@ -18,4 +18,31 @@ describe ('Missing Bits', () => {
 
         expect(actual).toEqual(expected);
     });
+
+    it ('handles an empty input', () => {
+        const input: number[] = [];
+        const expected: string[] = [];
+
+        const actual = missingBits(input);
+
+        expect(actual).toEqual(expected);
+    });
+
+    it ('handles a single element input', () => {
+        const input: number[] = [10];
+        const expected: string[] = ['10'];
+
+        const actual = missingBits(input);
+
+        expect(actual).toEqual(expected);
+    });
+
+    it ('handles a decending input', () => {
+        const input = [2, 1, -1, -2, -5];
+        const expected = ['2', '1', '0', '-1', '-2', '...', '-5'];
+
+        const actual = missingBits(input);
+
+        expect(actual).toEqual(expected);
+    });
 });
--- src/missing-bits/missing-bits.ts
+++ src/missing-bits/missing-bits.ts
@@ -1,3 +1,49 @@
 export const missingBits = (input: number[]): string[] => {
-    return [];
+    // Iterate through the list with two pointers
+    // Compare previous to current
+    // If abs(current - previous) === 2, insert the number between current and previous into the output
+    // If abs(current - previous) > 2, insert '...' into the output
+    // Using abs so that it works on decending lists too.
+
+    const output: string[] = [];
+
+    // If the input is empty, skip all of this.
+    if (input.length !== 0) {
+        // Add first element to the output
+        output.push(input[0].toString());
+
+        // Start looping from index 1
+        let previousIndex = 0;
+        let currentIndex = 1;
+        while(currentIndex < input.length) {
+            const difference = Math.abs(input[currentIndex] - input[previousIndex]);
+
+            switch (difference) {
+            // If the difference is 1, then the sequence is unbroken. Insert the current number.
+            case 1: {
+                output.push(input[currentIndex].toString());
+                break;
+            }
+            // If the difference is 2, insert the missing number then the current number
+            case 2: {
+                const middle = findNumberBetween(input[currentIndex], input[previousIndex]);
+                output.push(middle.toString());
+                output.push(input[currentIndex].toString());
+                break;
+            }
+            // Otherwise, insert an elipsis followed by the current number
+            default: {
+                output.push('...');
+                output.push(input[currentIndex].toString());
+                break;
+            }}
+
+            previousIndex++;
+            currentIndex++;
+        }
+    }
+
+    return output;
 };
+
+const findNumberBetween = (input1: number, input2: number): number => (input1 + input2) / 2;
