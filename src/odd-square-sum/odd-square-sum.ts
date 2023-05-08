df9c957279691b9adc81ebd5b60dad6eef5fb48c
Brian Manley <brian.manley@wework.com>
2023-05-08 08:41:21 -0400

    Added solution for odd square sum

--- src/odd-square-sum/odd-square-sum.ts
+++ src/odd-square-sum/odd-square-sum.ts
@@ -1,3 +1,20 @@
 export const oddSquareSum = (input: number): number => {
-    return input;
+    // Start with running total of 0
+    let sum = 0;
+
+    // Since Odd Squares are squares of odd numbers, we can save time by only looking at odds
+    // Start at 1 and add 2 when we iterate in the loop below
+    let currentOdd = 1;
+    let currentOddSquared = currentOdd * currentOdd;
+
+    while (currentOddSquared < input) {
+        // Add to running total
+        sum += currentOddSquared;
+
+        // Iterate
+        currentOdd += 2;
+        currentOddSquared = currentOdd * currentOdd;
+    }
+
+    return sum;
 };
